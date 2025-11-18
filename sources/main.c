fa5f7d2fd016202390570d74226057b770ead9fe
Matis <matis.mikael@gmail.com>
2025-11-18 18:28:14 +0100

    update menu, now be able to exit, add rule to knowledge base and print knowledge base

--- sources/main.c
+++ sources/main.c
@@ -2,6 +2,7 @@
 
 #include "safe-get.h"
 #include "rule.h"
+#include "knowledge-base.h"
 
 /*
  * 	PD	(Proposition Déduite)
@@ -23,79 +24,98 @@
  *				ajouter à PD la conclusion de R
  * */
 
+void start_menu(KnowledgeBase *kb, unsigned long long int* usr_input);
+void exit_menu();
+
 void clear_shell() { 
 	printf("\033[2J\033[H");
 	fflush(stdout);
 }
 
-void add_rule_menu()
+void add_rule_menu(KnowledgeBase *kb, unsigned long long int* usr_input)
 {
 	clear_shell();
-	printf(
-		"[===============Add Rule================]\n"
-	);
-	
-}
-void start_menu(unsigned long long int* usr_input) 
-{
-	clear_shell();
-	printf(
-		"[===============Expert System===========]\n"
-		"|      (0) Exit.                        |\n"
-		"|      (1) Add rule.                    |\n"
-		"[=======================================]\n"
-	);
-
+	printf("[===============Add Rule================]\n");
+	Rule r = rule_new();
+	char input[256];
 	do {
-                printf(">>> ");
+		printf("\nAdd a premise >>> ");
+
+		safe_edit_rule(&r, input, rule_add_premise);
+
+		do {
+			printf("Add another premise ? no(0) yes(1) >>> ");
+		}
+		while(safe_get_uint(usr_input) != 0);
+
+	} while(*usr_input != 0);
+
+	printf("\nAdd the conclusion >>> ");
+	safe_edit_rule(&r, input, rule_add_conclusion);
+
+	*kb = kb_add_rule(*kb, r);
+
+	printf("\n");
+	rule_print(r);
+	printf("\n\n");
+	do {
+                        printf(
+				"Add another Rule   (0)\n"
+				"Back to start menu (1)\n"
+				">>> ");
         }
         while(safe_get_uint(usr_input) != 0);
+
+	if(*usr_input == 0) { add_rule_menu(kb, usr_input); }
+	else {start_menu(kb, usr_input);}
+
+}
+void start_menu(KnowledgeBase *kb, unsigned long long int* usr_input) 
+{
+	clear_shell();
+	printf(
+			"[===============Expert System===========]\n"
+			"|      (0) Exit.                        |\n"
+			"|      (1) Add rule.                    |\n"
+			"|      (2) Show knowledge base          |\n"
+			"[=======================================]\n"
+	      );
+
+	do {
+		printf(">>> ");
+	}
+	while(safe_get_uint(usr_input) != 0);
+
+	switch(*usr_input)
+	{
+		case 0: exit_menu(); break;
+		case 1: add_rule_menu(kb, usr_input); break;
+		case 2:	
+			for(KnowledgeBase i = *kb; i != NULL; i = i->next) 
+			{ 
+				rule_print(i->rule); 
+			}
+	}
 }
 
 void exit_menu()
 {
 	clear_shell();
 	printf(
-		"[===================Exit=================]\n"
-		"| Thank you for using our expert system. |\n"
-		"[========================================]\n"
-	);
+			"[===================Exit=================]\n"
+			"| Thank you for using our expert system. |\n"
+			"[========================================]\n"
+	      );
 }
 
 
 void cli_interface()
 {
 	unsigned long long int usr_input;	
-//	KnowledgeBase kb = kb_new();
+	KnowledgeBase kb = kb_new();
 
-	start_menu(&usr_input);
+	start_menu(&kb, &usr_input);
 
-	switch(usr_input)
-	{
-		case 0: exit_menu(); break;
-		case 1: 
-
-			add_rule_menu();
-			
-			Rule r = rule_new();
-			char input[256];
-			do {	
-				printf("\nAdd a premise >>> ");
-			
-				safe_edit_rule(&r, input, rule_add_premise);
-
-				do {
-					printf("Add another premise ? no(0) yes(1) >>> ");
-        			}
-        			while(safe_get_uint(&usr_input) != 0);
-
-			} while(usr_input != 0);
-			
-			printf("\nAdd the conclusion >>> ");
-			safe_edit_rule(&r, input, rule_add_conclusion);
-			printf("\n");
-			rule_print(r);
-	}
 }
 
 int main()
